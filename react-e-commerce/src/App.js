5b8d561db298cf7bf0b21adab5022eaed70e18c0
AnandAK07 <anandkumark055@gmail.com>
2024-01-04 01:27:04 +0530

    productlist

---
+++ react-e-commerce/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+import ProductList from './features/counter/ProductList';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductList/>
+
+    </div>
+  );
+}
+
+export default App;
